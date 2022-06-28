04b5b4fa02d8e843e279210e5fbd02fd6ec6a4f9
Gaurav <gk44035@gmail.com>
2022-06-28 14:41:29 +0530

    home_

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="container">
+    <h1>DAILY ROUTINE</h1>
+    <form onSubmit={submitHandler}>
+        <input type="text" placeholder="Title" value={title} 
+        onChange={(e)=>setTitle(e.target.value)}
+        />
+        <textarea placeholder="Description"
+        value={description}
+        onChange={(e)=>setDescription(e.target.value)}
+        ></textarea>
+        <button type="submit">ADD</button>
+    </form>
+    {tasks.map((item,index) => (
+     <Task key={index} title={item.title} description={item.description}
+     deleteTask={deleteTask}
+     index={index}
+     />    
+   )) }
+</div>
+)
   )
 }
 
