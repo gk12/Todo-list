385626a37358c14f1bd68c5143a6171968cbc032
Gaurav <gk44035@gmail.com>
2022-06-29 11:00:22 +0530

    about_

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Header from "./Component/Header";
 import Home from "./Component/Home";
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import Contact from "./Component/Contact";
+import About from"./Component/About";
 function App() {
   return (
-    <div>
-      hello
-      <Header/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+        <Header />
 
+    <Routes>
+
+    <Route path='/' element={<Home />}/>
+    <Route path='/about' element={<About />}/>
+    <Route path='/contact' element={<Contact />}/>
+
+    </Routes>
+   </BrowserRouter>
   );
 }
 export default App;
