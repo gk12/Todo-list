848d5b08d303278aeb688d3d3555534196d6739d
Gaurav <gk44035@gmail.com>
2022-06-29 20:30:33 +0530

    user

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import Contact from "./Component/Contact";
 import About from"./Component/About";
 import Footer from './Component/Footer';
+import User from './Component/User';
 function App() {
   return (
     <Router>
-        <Header />
+    <Header />
 
     <Routes>
 
     <Route path='/' element={<Home />}/>
     <Route path='/about' element={<About />}/>
     <Route path='/contact' element={<Contact />}/>
+    <Route path='/user' element={User}/>
 
     </Routes>
     <Footer/>
