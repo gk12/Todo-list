c8abc7332b55cec36f3b60e43207d8e68a271b55
Gaurav <gk44035@gmail.com>
2022-06-29 11:10:44 +0530

    head

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,9 +1,17 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return <>
     <nav>Get Ready to Complete Your Daily Goals</nav>
+    <div className="header">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to='/contact'>Contact</Link>
+
+     
+    </div>
 
   </>
 };
